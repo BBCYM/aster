a3a2672b589eda5554ae2207eb7f41f49b18de39
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-07-05 22:14:26 +0800

    ready to fully merge

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-
+import 'react-native-gesture-handler'
 import {AppRegistry} from 'react-native';
-import App from './bot';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 
