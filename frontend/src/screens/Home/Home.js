994cc0e593debd0f6e3479c0e0f47523bb1e387e
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-07-05 23:10:15 +0800

    add gallary

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -3,8 +3,11 @@ import { Text, View } from 'react-native'
 import { AuthContext } from '../../contexts/AuthContext'
 
 
-export function HomeScreen() {
+export function HomeScreen(props) {
     // const {state} = React.useContext(AuthContext)
+    let change = ()=>{
+        props.navigation.replace('xxx')
+    }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
