c8e970e9dde2e448e51e7721524092ad883a53a8
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-07-07 17:48:28 +0800

    chatbot header temp done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
 import { AuthStackNavigator } from './navigators/AuthStackNavigators'
 import { PagesTabNavigator } from './navigators/PagesTabNavigator'
 import { AuthContext } from './contexts/AuthContext'
@@ -19,21 +19,21 @@ export default function () {
         <MainStack.Navigator
           screenOptions={{
             headerShown: false,
-            animationEnabled: false
+            animationEnabled:false
           }}
         >
           {
             state.splash ? (
               <MainStack.Screen name='Splash' component={SplashScreen} />
             ) : (
-              state.user ? (
-                console.log("hello user"),
-                <MainStack.Screen name='Pages' component={PagesTabNavigator} />
-              ) : (
-                console.log('need auth'),
-                <MainStack.Screen name='Auth' component={AuthStackNavigator} />
+                state.user ? (
+                  console.log("hello user"),
+                  <MainStack.Screen name='Pages' component={PagesTabNavigator} />
+                ) : (
+                    console.log('need auth'),
+                    <MainStack.Screen name='Auth' component={AuthStackNavigator} />
+                  )
               )
-            )
           }
         </MainStack.Navigator>
       </NavigationContainer>
---
+++ frontend/src/utils/switchtoChat.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+export default function switchChat(props){
+
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
