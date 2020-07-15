08a378e96128ab78abd1ed2127aa73f0a7d1374a
賴郁芳 <michelle860930860930@gmail.com>
2020-07-15 13:50:15 +0800

    你的branchname fix conflict and merge into master

--- frontend/index.js
+++ frontend/index.js
@@ -8,13 +8,3 @@ import App from './src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-
-const images = {
-    pic: {
-        camera: require('./src/pic/camera.png'),
-        person: require('./src/pic/person.png'),
-    },
-    accountpeople: require('./src/pic/accountpeople.png'),
-};
-
-export default images;
