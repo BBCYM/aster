709954badf50eff2880be76081fcb384613ebb71
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-07-21 14:53:37 +0800

    gallery still working

---
+++ frontend/src/components/photoComponent.js
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+} from 'react-native'
+import ActionButton from 'react-native-action-button';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+export default function photoFooter(that) {
+    return (
+        <View style={styles.root}>
+            <ActionButton
+                buttonColor="rgba(231,76,60,1)"
+                position='right'
+                offsetX={20}
+                offsetY={20}
+                fixNativeFeedbackRadius={true}
+                spacing={10}
+            >
+                <ActionButton.Item 
+                    buttonColor='#9b59b6' 
+                    title="New Tag" 
+                    spaceBetween={5}                    
+                    onPress={() => {that.setState({isTagModalVisi:true})}}
+                >
+                    <Ionicons name="pricetags-outline" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+            </ActionButton>
+        </View>
+    )
+}
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: screenWidth,
+        height: screenHeight * 0.2,
+        borderTopColor: '#FFFFFF',
+        borderTopWidth: 1
+    },
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    }
+})
