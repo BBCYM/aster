fe5320b7ff626745bb7e6a94c4d83284b3a095ed
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-07-31 02:19:53 +0800

    good bye djongo, hello mongoengine

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,10 +1,26 @@
-from djongo import models
-import datetime
+# from djongo import models
+# import datetime
+from datetime import datetime
+from mongoengine import Document, fields, signals
 
-class User(models.Model):
-    userId = models.CharField(max_length=100,primary_key=True)
-    expiresAt = models.DateTimeField(null=True,blank=True)
-    refreshToken=models.CharField(max_length=512,blank=False)
-    lastUpdateTime=models.DateTimeField(auto_now=True)
-    lastSync=models.DateTimeField(null=True, blank=True)
-    isSync=models.BooleanField(default=False)
+
+class User(Document):
+    userId = fields.StringField()
+    expiresAt = fields.DateTimeField()
+    refreshToken = fields.StringField()
+    lastUpdateTime = fields.DateTimeField(default=datetime.utcnow())
+    lastSync = fields.DateTimeField()
+    isSync = fields.BooleanField(default=False)
+
+    @classmethod
+    def pre_save(cls, sender, document):
+        document.lastUpdateTime = datetime.utcnow()
+
+
+signals.pre_save.connect(User.pre_save, sender=User)
+# userId = models.CharField(max_length=100,primary_key=True)
+# expiresAt = models.DateTimeField(null=True,blank=True)
+# refreshToken=models.CharField(max_length=512,blank=False)
+# lastUpdateTime=models.DateTimeField(auto_now=True)
+# lastSync=models.DateTimeField(null=True, blank=True)
+# isSync=models.BooleanField(default=False)
