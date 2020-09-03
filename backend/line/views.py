646f4a96d69351417ad9aff5834b204cdb3148de
yojin <annie19990306@gmail.com>
2020-09-03 23:38:23 +0800

    fix token error and line api linked

--- backend/line/views.py
+++ backend/line/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+import os 
+
+from django.conf import settings
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookParser
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
-import os 
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
   
 # Get the value of 'HOME' 
 # environment variable 
@@ -15,31 +16,60 @@ secret = 'LINE_CHANNEL_SECRET'
 secret_value = os.getenv(secret) 
 
 line_bot_api = LineBotApi(token_value)
-parser = WebhookParser(secret_value)
+handler = WebhookHandler(secret_value)
 
-# def callback(request):
-#     return HttpResponse("test!!")
 
 @csrf_exempt
-def callback(request):
-
-    if request.method == 'POST':
+def callback(request: HttpRequest) -> HttpResponse:
+    
+    if request.method == "POST":
+        # get X-Line-Signature header value
         signature = request.META['HTTP_X_LINE_SIGNATURE']
+
+        # get request body as text
         body = request.body.decode('utf-8')
 
+        # handle webhook body
         try:
-            events = parser.parse(body, signature)
+            handler.handle(body, signature)
         except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
             return HttpResponseBadRequest()
 
-        for event in events:
-            if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(
-                    event.reply_token,
-                   TextSendMessage(text=event.message.text)
-                )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event: MessageEvent):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+# def callback(request):
+#     return HttpResponse("test!!")
+
+# @csrf_exempt
+# def callback(request):
+
+#     if (request.method == 'POST'):
+#         signature = request.META['HTTP_X_LINE_SIGNATURE']
+#         body = request.body.decode('utf-8')
+
+#         try:
+#             events = parser.parse(body, signature)
+#         except InvalidSignatureError:
+#             return HttpResponseForbidden()
+#         except LineBotApiError:
+#             return HttpResponseBadRequest()
+
+#         for event in events:
+#             if isinstance(event, MessageEvent):
+#                 # print("event.message.text",event.message.text)
+#                 # line_bot_api.reply_message(
+#                 #     event.reply_token,
+#                 #    TextSendMessage(text=event.message.text)
+#                 # )
+#         return HttpResponse()
+#     else:
+#         return HttpResponseBadRequest()
