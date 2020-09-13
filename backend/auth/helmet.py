c5e4448bb2f08726ab5c47a1ea60ddedb7d2d01d
schuaBob <31004842+schuaBob@users.noreply.github.com>
2020-09-13 23:42:13 +0800

    try this time

--- backend/auth/helmet.py
+++ backend/auth/helmet.py
@@ -2,10 +2,12 @@ from django.core.handlers.wsgi import WSGIRequest
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import get_class_that_defined_method
+from django.http import HttpResponseNotAllowed, HttpResponseForbidden
 import os
 import base64
 import hashlib
 import hmac
+import inspect
 class AsterMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -34,14 +36,17 @@ class AsterMiddleware:
         elif XRequestedWith != self.app or Authorization != self.access_code:
             return ResWith401(request, view_func)
         return None
-def ResWith401(request, view_func):
-    res = Response('Reuqest not authorized.', status=status.HTTP_401_UNAUTHORIZED)
-    if not getattr(request, 'accepted_renderer', None):
-        viewclass = get_class_that_defined_method(view_func)
-        neg = viewclass().perform_content_negotiation(request, force=True)
-        request.accepted_renderer, request.accepted_media_type = neg
-        res.accepted_renderer = request.accepted_renderer
-        res.accepted_media_type = request.accepted_media_type
-        res.renderer_context = viewclass().get_renderer_context()
+def ResWith401(request, view_func=None):
+    res = HttpResponseForbidden('Reuqest not authorized.')
+    # res = Response('Reuqest not authorized.', status=status.HTTP_401_UNAUTHORIZED)
+    # if view_func.__name__ == 'callback':
+    #     return res    
+    # if not getattr(request, 'accepted_renderer', None):
+    #     viewclass = get_class_that_defined_method(view_func)
+    #     neg = viewclass().perform_content_negotiation(request, force=True)
+    #     request.accepted_renderer, request.accepted_media_type = neg
+    #     res.accepted_renderer = request.accepted_renderer
+    #     res.accepted_media_type = request.accepted_media_type
+    #     res.renderer_context = viewclass().get_renderer_context()
     return res
 
